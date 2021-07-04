672bcca3fc7ec54f8a05c37b8ad445caf6e6fccd
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-04 18:56:11 +0300

    creating the urls pattern

--- photos/urls.py
+++ photos/urls.py
@@ -4,3 +4,12 @@ from django.conf.urls.static import static
 from django.conf.urls import include
 from . import views
 from .views import search_category,home
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('search/', views.search_category, name='search_category'),
+    path('about/', views.about, name='about'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
