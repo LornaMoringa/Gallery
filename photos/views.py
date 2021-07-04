24bc374d27a03606ce88dfc9fea1ae8c8db3b9c8
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-04 19:02:49 +0300

    updatin views.py

--- photos/views.py
+++ photos/views.py
@@ -4,3 +4,7 @@ from .models import Image, Category, Location
 
 # Create your views here.
 
+def home(request):
+    images= Image.objects.all()
+    return render(request, "home.html", {"images":images})
+
