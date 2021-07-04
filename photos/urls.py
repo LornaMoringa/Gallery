1ad4c4ac2a2b4e151c8f89a712ef481b51366db8
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-07-04 18:55:05 +0300

    creating the image urls

---
+++ photos/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
+from . import views
+from .views import search_category,home
